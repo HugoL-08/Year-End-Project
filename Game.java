97f70303a38431d8f33c6cd8bf4b1278fe55e964
HugoL-08 <hugo@lattimore.id.au>
2026-06-08 11:04:20 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -2,22 +2,34 @@ import java.util.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Game {
+public class Game implements ActionListener {
 
     private static Player playerOne;
     private static Player playerTwo;
 
+    private static String turn;
+    private static String winner;
+
     private static JFrame mainFrame;
     private static JList guessList;
-    private static DefaultListModel<String> guessListModel;
+    private static DefaultListModel<String> playerOneListModel;
+    private static DefaultListModel<String> playerTwoListModel;
     private static JButton guessButton;
 
     public static void initalizeGame()
     {
         Card.initializeDeck();
 
+        turn = "playerOne";
+
         playerOne = new Player();
         playerTwo = new Player();
+
+        Card.dealCards(playerOne, 7);
+        Card.dealCards(playerTwo, 7);
+
+        playerOne.findPairs();
+        playerTwo.findPairs();
     }
 
     public static void initializeMainFrame()
@@ -27,14 +39,11 @@ public class Game {
         mainFrame.setLayout(null);
         mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         
-        guessListModel = new DefaultListModel<>();
-        for (String s : playerOne.getHand())
-        {
-            guessListModel.addElement(s);
-        }
-
-        guessList = new JList(guessListModel);
+        guessList = new JList();
         guessList.setBounds(20, 20, 300, 500);
+
+        playerOneListModel = new DefaultListModel<String>();
+        playerTwoListModel = new DefaultListModel<String>();
 
         guessButton = new JButton("Guess");
         guessButton.setBounds(400, 20, 100, 75);
@@ -42,6 +51,57 @@ public class Game {
         mainFrame.add(guessList);
         mainFrame.add(guessButton);
         mainFrame.setVisible(true);
+    }
+
+    public static void updateGuessList()
+    {
+        playerOneListModel.clear();
+        playerTwoListModel.clear();
+
+        for (String s : playerOne.getHand())
+        {
+            playerOneListModel.addElement(s);
+        }
+
+        for (String s : playerTwo.getHand())
+        {
+            playerTwoListModel.addElement(s);
+        }
+
+        if (turn.equals("playerOne"))
+        {
+            guessList.setModel(playerTwoListModel);
+            turn = "playerTwo";
+        }
+
+        if (turn.equals("playerTwo"))
+        {
+            guessList.setModel(playerOneListModel);
+            turn = "playerOne";
+        }
+    }
+
+    public static void switchTurn()
+    {
+        
+    }
+
+    public static void determineWinner()
+    {
+        if (playerOne.getScore() > playerTwo.getScore())
+        {
+            winner = "Congratulations Player One, you have won!";
+        }
+
+        if (playerTwo.getScore() > playerOne.getScore())
+        {
+            winner = "Congratulations Player Two, you have won!";
+        }
+
+        else
+        {
+            winner = "Sorry, both players have tied.";
+        }
     }
     
     public static void testGame()
